a3d0a3230363ccd8e7333887fa0f8856ab0e40b9
CybertekSchool <hhamidz1100@gmail.com>
2018-07-14 15:58:28 -0400

    Using JavaScript

--- src/test/java/syn/FluentWaitDemo.java
+++ src/test/java/syn/FluentWaitDemo.java
@@ -5,7 +5,7 @@ import static org.testng.Assert.assertTrue;
 
 import java.time.Duration;
 import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.Test;
 
-import com.google.common.base.Function;
+
 
 public class FluentWaitDemo extends TestBase{
 
@@ -32,14 +32,14 @@ public class FluentWaitDemo extends TestBase{
 				.pollingEvery(Duration.ofSeconds(1))
 				
 				.ignoring(NoSuchElementException.class);
-		
+		// old way
 //		WebElement element = wait.until(
 //				new Function<WebDriver, WebElement>(){
 //			public WebElement apply(WebDriver driver) {
 //				return driver.findElement(By.id("message"));
 //		}
 //	});
-
+		  // the condition which the wait object will wait for
    WebElement message = wait.until(dr -> dr.findElement(By.id("message")));	
 		
 		assertTrue(message.isDisplayed());
